74f62619c63f398f6139b403597bd1d637dfa277
einsteinhx <einsteinhx@gmail.com>
2014-07-09 19:29:44 +0800

    nav optimize

--- app/assets/js/api.js
+++ app/assets/js/api.js
@@ -1,10 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+	//prettyPrint	
 	var pres = document.querySelectorAll("pre");
-
-	[].forEach.call(pres, function(pre){
-		pre.classList.add("prettyprint");
-	});
-	prettyPrint();
+	if(pres.length){
+		[].forEach.call(pres, function(pre){
+			pre.classList.add("prettyprint");
+		});	
+		prettyPrint();
+	}
+	
+	//generate icon
+	var generateIcon = function(){
+		var menu = document.querySelector("#menu");
+		var dds = menu.querySelectorAll("dd");
+		if(dds.length){
+			[].forEach.call(dds, function(dd){
+				var frag = document.createDocumentFragment();
+				var i = document.createElement("i");
+				i.classList.add("fa");
+				i.classList.add("fa-angle-double-right");
+				frag.appendChild(i);
+				dd.insertBefore(frag, dd.firstChild);
+			});	
+		}
+	}
+	//generateIcon();
 
 }, false);
