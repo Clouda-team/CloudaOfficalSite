24d4cb772a5ebb1d4343d9be2e7d1b8e598b1dd4
einsteinhx <einsteinhx@gmail.com>
2014-07-21 20:56:21 +0800

    dynamic github stars and forks

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,11 +1,63 @@
 var server = rapid.use("rapid-httpserver");
+var https = require("https");
+var fetchInterval = 24 * 60 * 60 * 1000;
 
-server.defineAction("index", function(default_request, default_response){
+var github = {
+	stars : 0,
+	forks : 0
+};
 
-	var req = default_request;
-	var res = default_response;
+function fetchGithubStars (org){
 
-	var content = this.render("index");
+	var opts = {
+		hostname : "api.github.com",
+		path : "/orgs/" + org + "/repos",
+		headers : {
+			"User-Agent" : "CloudaTeam"
+		}
+	}
+
+	var req = https.request(opts, function(res){
+		var data = "";
+		res.on('data', function(chunk){
+			data += chunk;
+		});
+
+		res.on('end', function() {
+			data = data.toString();
+			try{
+
+				var orgdata = JSON.parse(data);
+				var stars = 0, forks = 0;
+				orgdata.forEach(function(item){
+					stars += item.stargazers_count;
+					forks += item.forks_count;
+				});
+
+				github.stars = stars;
+				github.forks = forks;
+
+			}catch(e){
+				console.log(e);
+			}
+		});
+	});
+
+	req.on("error", function(err){
+		console.log(err);
+	});
+
+	req.end();
+
+}
+
+//fetch
+fetchGithubStars("Clouda-team");
+setInterval(function(){ fetchGithubStars("Clouda-team"); }, fetchInterval);
+
+server.defineAction("index", function(){
+
+	var content = this.render("index", github);
 
 	this.send(content);
 	
